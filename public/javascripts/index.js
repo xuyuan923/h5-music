cdc39b329bb71f8acca8a979e12d0fda900bb28f
xuyuan923 <xuyuan923@163.com>
2015-01-22 11:40:10 +0800

    up

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -18,18 +18,42 @@ for(var i = 0; i < list.length; i++){
 
 var xhr = new XMLHttpRequest();
 var ac = new (window.AudioContext||window.webkitAudioContext)();
+var gainNode = ac[ac.createGain?"createGain":"createGainNode"]();
+gainNode.connect(ac.destination);
+
+var source = null;
+var count = 0;
 function load(url){
+    var n = ++count;
+    //当前一首歌停止播放后一首歌
+    source && source[source.stop?"stop":"noteOff"]();
+    xhr.abort();
     xhr.open("GET",url);
     xhr.responseType = 'arraybuffer';
     xhr.onload = function(){
+        if(n != count) return;
         ac.decodeAudioData(xhr.response,function(buffer){
+            if(n != count) return;
             var bufferSource = ac.createBufferSource();
             bufferSource.buffer = buffer;
+            bufferSource.connect(gainNode);
             bufferSource.connect(ac.destination);
             bufferSource[bufferSource.start?"start":"noteOn"](0);
+            source = bufferSource;
         },function(err){
             console.log(err);
         })
     }
     xhr.send();
 }
+
+function changeVolume(percent){
+    gainNode.gain.value = percent * percent;
+}
+
+$('#volume')[0].onchange = function(){
+    console.log('change');
+    changeVolume(this.value/this.max);
+};
+
+//$('#volume')[0].onchange();
